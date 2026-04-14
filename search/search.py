0e46e23fa15c5ded966f493e612b35ac851e1dc9
Nikola <gudzic.nikola@hotmail.com>
2026-04-14 22:57:27 +0200

    parsiranje godine iz upita

--- search/search.py
+++ search/search.py
@@ -3,35 +3,43 @@ from etl.db import get_connection
 
 
 def embed_query(query: str):
-    vector = model.encode(
+    return model.encode(
         f"query: {query.strip()}",
         normalize_embeddings=True
-    )
-    return vector.tolist()
+    ).tolist()
 
 
-def semantic_search(query: str, limit: int = 10):
+def semantic_search(query: str, limit: int = 10, year_from: int | None = None):
     conn = get_connection()
     query_vector = embed_query(query)
 
+    base_query = """
+        SELECT
+            id,
+            title,
+            source_url,
+            date,
+            embedding <-> %s::vector AS distance
+        FROM publication
+        WHERE embedding IS NOT NULL
+    """
+
+    params = [query_vector]
+
+    # dodavanje filtera za godinu
+    if year_from:
+        base_query += " AND date >= %s"
+        params.append(f"{year_from}-01-01")
+
+    # redosled
+    base_query += " ORDER BY embedding <-> %s::vector LIMIT %s"
+
+    params.append(query_vector)
+    params.append(limit)
+
     with conn.cursor() as cur:
-        cur.execute(
-            """
-            SELECT
-                id,
-                title,
-                abstract,
-                source_url,
-                date,
-                embedding <-> %s::vector AS distance
-            FROM publication
-            WHERE embedding IS NOT NULL
-            ORDER BY embedding <-> %s::vector
-            LIMIT %s
-            """,
-            (query_vector, query_vector, limit),
-        )
-        rows = cur.fetchall()
+        cur.execute(base_query, params)
+        results = cur.fetchall()
 
     conn.close()
-    return rows
+    return results
--- test_search.py
+++ test_search.py
@@ -1,13 +1,10 @@
 from search.search import semantic_search
 
-results = semantic_search("semantic web ontologies", limit=10)
+results = semantic_search(
+    "semantic web",
+    limit=10,
+    year_from=2024
+)
 
 for row in results:
-    pub_id, title, abstract, source_url, date, distance = row
-
-    print("-" * 80)
-    print("ID:", pub_id)
-    print("Title:", title)
-    print("Date:", date)
-    print("Distance:", distance)
-    print("URL:", source_url)
+    print(row)
